6062c097531cf59d7ec9dac75df83f8710d7db1b
Franco Lippi <soyflippi@gmail.com>
2023-03-18 20:11:35 -0300

    first commit

---
+++ app/index.php
@@ -0,0 +1,29 @@
+<?php
+// Error Handling
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Routing\RouteCollectorProxy;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Instantiate App
+$app = AppFactory::create();
+
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Add parse body
+$app->addBodyParsingMiddleware();
+
+// Routes
+$app->get('[/]', function (Request $request, Response $response) {    
+    $response->getBody()->write("GET => Bienvenido!!! a SlimFramework");
+    return $response;
+
+});
+
+$app->run();
